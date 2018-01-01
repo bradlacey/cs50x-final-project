8bb99f3cd042e8fb8ffc4f6e8e29fa73baf42b64
Brad Lacey <bradlacey@gmail.com>
2018-01-01 18:43:32 +1100

    login troubleshooting 10 - app.config["SESSION_TYPE"] = "memcached"

--- application.py
+++ application.py
@@ -86,12 +86,13 @@ app.jinja_env.filters["usd"] = usd
 
 
 # configure session to use filesystem (instead of signed cookies)
-# login troubleshooting 7, 8, 9
+# login troubleshooting 7, 8, 9, 10
 # app.config["SESSION_FILE_DIR"] = mkdtemp()
 # app.config["SESSION_PERMANENT"] = False
 # app.config["SESSION_PERMANENT"] = True
 # app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_TYPE"] = "sqlalchemy"
+# app.config["SESSION_TYPE"] = "sqlalchemy"
+app.config["SESSION_TYPE"] = "memcached"
 Session(app)
 
 
