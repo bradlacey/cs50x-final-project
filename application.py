6d497e49336e93effbf53277185a44d075e46cc7
Brad Lacey <bradlacey@gmail.com>
2018-01-01 18:22:15 +1100

    login troubleshooting 8 - mishmash

--- application.py
+++ application.py
@@ -91,10 +91,10 @@ app.secret_key = "doogiehowser"
 
 
 # configure session to use filesystem (instead of signed cookies)
-# login troubleshooting 7
+# login troubleshooting 7, 8
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 # app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_PERMANENT"] = True
+# app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
